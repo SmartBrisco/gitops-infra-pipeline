f7c44743d2d7a34db501f95f1d20389d637bfd66
Brian Scott <smartbrisco@gmail.com>
2026-03-18 15:29:25 -0500

    update provider for aws to include skip flags

--- terraform/aws/providers.tf
+++ terraform/aws/providers.tf
@@ -1,10 +1,10 @@
-terraform {
+hclterraform {
   required_version = ">= 1.7.0"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.85"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
@@ -12,6 +12,6 @@ terraform {
 provider "aws" {
   region                      = var.aws_region
   skip_credentials_validation = true
-  skip_requesting_account_id = true
+  skip_requesting_account_id  = true
   skip_metadata_api_check     = true
 }
