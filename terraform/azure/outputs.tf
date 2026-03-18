3dde6052bba7f08dc8cc33c121bd00d60e05fcd7
Brian Scott <smartbrisco@gmail.com>
2026-03-18 11:33:40 -0500

    updated azure file name

---
+++ terraform/azure/outputs.tf
@@ -0,0 +1,14 @@
+output "vm_name" {
+  description = "Azure VM name"
+  value       = var.deploy ? azurerm_linux_virtual_machine.vm[0].name : "not deployed"
+}
+
+output "vm_private_ip" {
+  description = "Azure VM private IP"
+  value       = var.deploy ? azurerm_network_interface.nic[0].private_ip_address : "not deployed"
+}
+
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = var.deploy ? azurerm_resource_group.rg[0].name : "not deployed"
+}
