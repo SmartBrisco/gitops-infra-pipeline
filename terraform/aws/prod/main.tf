57367cf60d1ff034ec9011da8ec0151ec334ab2b
SmartBrisco <smartbrisco@gmail.com>
2026-03-21 22:02:39 -0500

    Feat/correct (#30)
    updating to pass trivy for prod

--- terraform/aws/prod/main.tf
+++ terraform/aws/prod/main.tf
@@ -4,18 +4,18 @@ resource "aws_security_group" "prod" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "SSH"
+    description = "Internal only"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   tags = {
@@ -30,6 +30,10 @@ resource "aws_instance" "prod" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.prod.id]
+
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name        = "${var.project_name}-prod-instance"
