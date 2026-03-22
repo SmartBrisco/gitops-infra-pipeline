a82d4c65b1c817a9ff96a8daf99df74565e8b21a
Brian Scott <smartbrisco@gmail.com>
2026-03-21 21:57:46 -0500

    cleanup main

--- terraform/aws/prod/main.tf
+++ terraform/aws/prod/main.tf
@@ -1,20 +1,24 @@
 resource "aws_security_group" "prod" {
-    name        = "${var.project_name}-prod-sg"
+  name        = "${var.project_name}-prod-sg"
   description = "security group for gitops pipeline prod"
   vpc_id      = aws_vpc.main.id
-  ingress {
-    cidr_blocks = ["10.0.0.0/8"]  # internal only
-  }
-  egress {
-    cidr_blocks = ["10.0.0.0/8"]  # internal only
-  }
-}
 
-resource "aws_instance" "prod" {
-  root_block_device {
-    encrypted = true
+  ingress {
+    description = "Internal only"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
   }
-    tags = {
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  tags = {
     Name        = "${var.project_name}-prod-sg"
     managed-by  = "terraform"
     environment = "prod"
@@ -22,13 +26,14 @@ resource "aws_instance" "prod" {
 }
 
 resource "aws_instance" "prod" {
-  root_block_device {
-    encrypted = true
-  }
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.prod.id]
+
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name        = "${var.project_name}-prod-instance"
